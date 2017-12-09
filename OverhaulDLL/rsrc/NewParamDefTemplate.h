0d734901f781b0212a56ee999860d4c39e6a1239
SeanPesce <pesce.sean@gmail.com>
2017-12-09 18:40:42 -0500

    Update resource files

--- OverhaulDLL/rsrc/NewParamDefTemplate.h
+++ OverhaulDLL/rsrc/NewParamDefTemplate.h
@@ -1,73 +1,73 @@
 /*
-	DARK SOULS OVERHAUL
+    DARK SOULS GAME FILE C++ LIBRARY
 
-	Contributors to this file:
-		Sean Pesce	-	C++
-
-
-	ParamDef/NewParamDef.h
-
-	New Parameters from NewParam.paramdef
+    Contributors to this file:
+        Sean Pesce    -    C++
 
 
-	References:
-		- (Author?) ParamDefs Sheet
-		  https://docs.google.com/spreadsheets/d/1KukblWL61We64-gNIyaAShga9h8RTXYmyFs98eQhY4E/
+    Param/New.h
 
-		- Kushisushi's ParamDefs Sheet
-		  https://docs.google.com/spreadsheets/d/1QX6i93H4_jGZ0So9mArDECnuv1lfkaJQhXOzkDuxdkE/edit#gid=2133439974
+    New Parameters from NewParam.paramdef
+
+
+    References:
+        - Burton Radons' ParamDefs Sheet
+          https://docs.google.com/spreadsheets/d/1KukblWL61We64-gNIyaAShga9h8RTXYmyFs98eQhY4E/
+
+        - Kushisushi's ParamDefs Sheet
+          https://docs.google.com/spreadsheets/d/1QX6i93H4_jGZ0So9mArDECnuv1lfkaJQhXOzkDuxdkE/edit#gid=2133439974
 
 */
 
 #pragma once
 
 #ifndef _DS1_OVERHAUL_NEW_PARAM_DEF_FILE_H_
-	#define _DS1_OVERHAUL_NEW_PARAM_DEF_FILE_H_
+    #define _DS1_OVERHAUL_NEW_PARAM_DEF_FILE_H_
 
 
-#include "GameParamDef.h"
+#include "Param.h"
 
 
 // New parameter
 typedef struct NewParameter : public Param {
-	@TODO
+    @TODO
 } NewParam;
 
 
 
 // New parameter definitions file
-class NewParamDef : public GameParamDef {
+class NewParamDef : public ParamDef {
 
 
 public:
-	static NewParamDef& get_instance()
-	{
-		static NewParamDef instance;
-		return instance;
-	}
-	
-	NewParam *data()
-	{
-		return (NewParam*)GameParamDef::data();
-	}
+    static NewParamDef& get_instance()
+    {
+        static NewParamDef instance;
+        return instance;
+    }
+    
+    NewParam *data()
+    {
+        return (NewParam*)ParamDef::data();
+    }
 
-	NewParam *get(int index)
-	{
-		return (NewParam*)GameParamDef::get(index);
-	}
+    NewParam *get(int index)
+    {
+        return (NewParam*)ParamDef::get(index);
+    }
 
 
-	// @TODO: Member data specific to this class
-	
+    // @TODO: Member data specific to this class
+    
 private:
-	NewParamDef()
-		: GameParamDef(/* @TODO: NULL, 0xdata_start_offset, param_count, sizeof(NewParam), "scan_pattern", "NewParam", "title" */)
-	{
-	}
-	
+    NewParamDef()
+        : ParamDef(/* @TODO: NULL, sizeof(NewParam), "scan_pattern", "NewParam", "title" */)
+    {
+    }
+    
 public:
-	NewParamDef(NewParamDef const&) = delete;
-	void operator=(NewParamDef const&) = delete;
+    NewParamDef(NewParamDef const&) = delete;
+    void operator=(NewParamDef const&) = delete;
 };
 
 
