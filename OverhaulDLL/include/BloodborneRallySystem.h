1aca66f6cab631c22174c2c05f818906ed35d8da
metal-crow <frankcrisanti@aol.com>
2017-12-16 04:20:07 -0500

    Inital porting of bb rally system to c++. untested

---
+++ OverhaulDLL/include/BloodborneRallySystem.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef _DS1_OVERHAUL_BB_RALLY_SYSTEM_H_
+#define _DS1_OVERHAUL_BB_RALLY_SYSTEM_H_
+
+
+#include <cstdint>
+
+
+class BloodborneRally
+{
+public:
+    static void start();
+private:
+    static const uint32_t weapon_toggle_injection_offset = 0xB37F31;
+    static void __stdcall weapon_toggle_injection();
+    static const uint32_t current_selected_bar_injection_offset = 0x386D04;
+    static void __stdcall selected_bar_injection();
+    static const uint32_t control_timer_injection_offset = 0x888CED;
+    static void __stdcall control_timer_injection();
+    static const uint32_t main_rally_injection_offset = 0xA6BFE6;
+    static void __stdcall main_rally_injection();
+}
+
+#endif //_DS1_OVERHAUL_BB_RALLY_SYSTEM_H_
