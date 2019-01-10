1e51d992d1b3c1d4c4a720e5de9282ba0035becc
metal-crow <frankcrisanti@aol.com>
2019-01-09 20:55:01 -0500

    binoculars anti-cheat and dragon changing anti-cheat added back

--- OverhaulDLL/src/AntiCheat/AntiCheat.cpp
+++ OverhaulDLL/src/AntiCheat/AntiCheat.cpp
@@ -40,24 +40,41 @@ void start() {
 
     // Start NpcGuard anti-cheat
     global::cmd_out << "    Enabling NpcGuard...";
-    Mod::startup_messages.push_back("    Enabling NpcGuard...");
     uint64_t write_address = Game::ds1_base + NpcGuard_offset;
     npc_guard_WorldChrBase = Game::world_char_base;
     sp::mem::code::x64::inject_jmp_14b((void*)write_address, &npc_guard_check_exit, 2, &npc_guard_asm_check, true);
 
     // Start BossGuard anti-cheat
     global::cmd_out << "    Enabling BossGuard...";
-    Mod::startup_messages.push_back("    Enabling BossGuard...");
     write_address = Game::ds1_base + BossGuard_offset;
     sp::mem::code::x64::inject_jmp_14b((void*)write_address, &boss_guard_return, 0, &boss_guard_asm_check);
 
     // Start TeleBackstabProtect anti-cheat
     global::cmd_out << "    Enabling TeleBackstabProtect...";
-    Mod::startup_messages.push_back("    Enabling TeleBackstabProtect...");
     write_address = Game::ds1_base + TeleBackstab_getBSAnimation_offset;
     sp::mem::code::x64::inject_jmp_14b((void*)write_address, &TeleBackstabProtect_store_AnimationId_return, 0, &TeleBackstabProtect_store_AnimationId);
     write_address = Game::ds1_base + TeleBackstab_setPlayerLocation_offset;
     sp::mem::code::x64::inject_jmp_14b((void*)write_address, &TeleBackstabProtect_setPosition_return, 5, &TeleBackstabProtect_setPosition_check);
+
+    // Start BinocsTriggerBlock anti-cheat
+    global::cmd_out << "    Enabling BinocsTriggerBlock...";
+    void* bino_params = sp::mem::aob_scan("00 0F 00 00 00 00 FF 00 7F");
+    if (bino_params == NULL) {
+        FATALERROR("Unable to activate BinocsTriggerBlock");
+    }
+    uint8_t bino_params_patched_bytes[1] = { 0x79 };
+    sp::mem::patch_bytes((void*)((uint64_t)bino_params + 8), bino_params_patched_bytes, 1);
+
+    // Start DragonTriggerBlock anti-cheat
+    global::cmd_out << "    Enabling DragonTriggerBlock...";
+    uint8_t dragon_params_patched_bytes[1] = { 0x79 };
+    void* dragon_head_params = sp::mem::aob_scan("60 00 00 00 00 FF 00 7F");
+    void* dragon_body_params = sp::mem::aob_scan("A7 00 00 00 00 FF 00 7F");
+    if (dragon_head_params == NULL || dragon_body_params == NULL) {
+        FATALERROR("Unable to activate DragonTriggerBlock");
+    }
+    sp::mem::patch_bytes((void*)((uint64_t)dragon_head_params + 7), dragon_params_patched_bytes, 1);
+    sp::mem::patch_bytes((void*)((uint64_t)dragon_body_params + 7), dragon_params_patched_bytes, 1);
 }
 
 } // namespace AntiCheat
