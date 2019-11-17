68006fb89510230f2ce8d4da918fe9ede27c139a
metal-crow <frankcrisanti@aol.com>
2019-11-17 16:46:28 -0600

    include c0000 in files to replace on load

--- OverhaulDLL/src/Files.cpp
+++ OverhaulDLL/src/Files.cpp
@@ -15,7 +15,8 @@ std::string Files::save_file;
 //Make sure this is lowercase
 std::set<std::wstring> Files::files_to_intercept_loading = {
     L"frpg_sfxbnd_commoneffects.ffxbnd.dcx",
-    L"gameparam.parambnd.dcx"
+    L"gameparam.parambnd.dcx",
+    L"c0000.esd.dcx"
 };
 
 int Files::string_wide_to_mb(wchar_t *in_string, std::string &out_string)
