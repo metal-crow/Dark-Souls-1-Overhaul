6b9b8259893581d09ffffa4181a838d77ce4fa25
metal-crow <frankcrisanti@aol.com>
2020-08-11 20:17:09 -0500

    fix ankles so they don't break when rolling

---
+++ OverhaulDLL/include/FixAnkles.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef _DS1_OVERHAUL_FIX_ANKLES_H_
+#define _DS1_OVERHAUL_FIX_ANKLES_H_
+
+#include <cstdint>
+
+
+class FixAnkles
+{
+public:
+    static void start();
+private:
+    static const uint64_t set_foot_ikvalue_injection_offset = 0x29eae9;
+};
+
+#endif
---
+++ OverhaulDLL/src/FixAnkles.cpp
@@ -0,0 +1,40 @@
+#include "FixAnkles.h"
+#include "DarkSoulsOverhaulMod.h"
+#include "SP/memory/injection/asm/x64.h"
+
+extern "C" {
+    uint64_t main_fix_ankles_injection_return;
+    void main_fix_ankles_injection();
+    void main_fix_ankles_helper(uint64_t parentfootIk);
+}
+
+void FixAnkles::start() {
+    global::cmd_out << Mod::output_prefix << "Enabling Ankle fix...\n";
+
+    //injection to change the foot ik type
+    uint8_t *write_address = (uint8_t*)(FixAnkles::set_foot_ikvalue_injection_offset + Game::ds1_base);
+    sp::mem::code::x64::inject_jmp_14b(write_address, &main_fix_ankles_injection_return, 1, &main_fix_ankles_injection);
+}
+
+void main_fix_ankles_helper(uint64_t parentfootIk) {
+    uint64_t id = *(uint64_t*)(*(uint64_t*)(*(uint64_t*)(*(uint64_t*)(*(uint64_t*)(parentfootIk) + 0x48) + 0x30 + 0x20) + 0x20));
+    uint8_t* footik_enabled = (uint8_t*)(parentfootIk + 0x180);
+
+    switch (id) {
+        //1h rolls
+        case 32:
+        case 33:
+        case 34:
+        case 35:
+        //2h rolls
+        case 94:
+        case 95:
+        case 96:
+        case 97:
+            *footik_enabled = 0;
+            return;
+        default:
+            *footik_enabled = 1;
+            return;
+    }
+}
