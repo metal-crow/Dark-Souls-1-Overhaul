705ea57a36555a3319eadffa6d3a373f339ab77c
metal-crow <frankcrisanti@aol.com>
2021-01-10 20:25:29 -0500

    clean up and remove unused features from moddata and ini options

--- OverhaulDLL/include/ModData.h
+++ OverhaulDLL/include/ModData.h
@@ -11,94 +11,22 @@
 #ifndef _DS1_OVERHAUL_MOD_DATA_H_
     #define _DS1_OVERHAUL_MOD_DATA_H_
 
-
-
-
 /////////////////// USER PREFERENCE & KEYBIND CONSTANTS ///////////////////
 #define _DS1_OVERHAUL_SETTINGS_FILE_ ".\\d3d11_mod.ini"
 #define _DS1_OVERHAUL_PREFS_SECTION_ "Dark Souls Overhaul Settings"
 #define _DS1_OVERHAUL_KEYBINDS_SECTION_ "Dark Souls Overhaul Keybinds"
-#define _DS1_OVERHAUL_INPUT_SECTION_ "Game Input"
-#define _DS1_OVERHAUL_ANTICHEAT_SECTION_ "Anti-Cheat"
-#define _DS1_OVERHAUL_HUD_SECTION_ "HUD"
-#define _DS1_OVERHAUL_DEBUG_SECTION_ "Dark Souls Overhaul Debug"
-#define _DS1_OVERHAUL_CHALLENGE_SECTION_ "Challenge Mods"
-#define _DS1_OVERHAUL_CHALLENGE_SETTINGS_SECTION_ "Challenge Mod Settings"
 // Startup preferences:
 #define _DS1_OVERHAUL_PREF_LEGACY_MODE_ "LegacyMode"
-#define _DS1_OVERHAUL_PREF_MEMORY_LIMIT_ "MemoryLimit"
 #define _DS1_OVERHAUL_PREF_CUSTOM_GAME_ARCHIVE_ "CustomArchiveFiles"
 #define _DS1_OVERHAUL_PREF_CUSTOM_SAVE_FILE_ "CustomSaveFile"
 #define _DS1_OVERHAUL_PREF_CUSTOM_GAME_CFG_FILE_ "CustomGameConfigFile"
-// Game input settings:
-#define _DS1_OVERHAUL_PREF_MOUSE_INPUT_ "MouseInput"
-#define _DS1_OVERHAUL_PREF_CONSOLE_LOCK_CAM_ "ConsoleLockCamera"
-// Anti-cheat:
-#define _DS1_OVERHAUL_PREF_AC_BOSS_GUARD_ "BossGuard"
-#define _DS1_OVERHAUL_PREF_AC_NPC_GUARD_ "NpcGuard"
-#define _DS1_OVERHAUL_PREF_AC_TELEBACKSTAB_PROT_ "TeleBackstabProtect"
-#define _DS1_OVERHAUL_PREF_AC_BINOCS_TRIGGER_BLOCK_ "BinocsTriggerBlock"
-#define _DS1_OVERHAUL_PREF_AC_DRAGON_TRIGGER_BLOCK_ "DragonTriggerBlock"
 // General settings:
-#define _DS1_OVERHAUL_PREF_SHOW_NODE_COUNT_ "DisplayNodeCount"
-#define _DS1_OVERHAUL_PREF_DIM_LAVA_ "DimLava"
-#define _DS1_OVERHAUL_PREF_DISABLE_ARMOR_SFX_ "DisableArmorSounds"
 #define _DS1_OVERHAUL_PREF_DISABLE_LOW_FPS_DISCONNECT_ "DisableLowFpsDisconnect"
-#define _DS1_OVERHAUL_PREF_DISABLE_GESTURE_CANCELLING_ "GestureCancelling"
-#define _DS1_OVERHAUL_PREF_CHEATS_WARNING_ "CheatsWarning"
-// HUD settings:
-#define _DS1_OVERHAUL_PREF_COMPASS_RADIAL_ "CompassRadial"
-#define _DS1_OVERHAUL_PREF_COMPASS_BAR_ "CompassBar"
-#define _DS1_OVERHAUL_PREF_ELEVATION_ "ElevationMeter"
-#define _DS1_OVERHAUL_PREF_NODE_GRAPH_ "NodeGraph"
-// Challenge mods:
-#define _DS1_OVERHAUL_PREF_CM_AGGRO_AI_ "AggressiveAI"
-#define _DS1_OVERHAUL_PREF_CM_BP_ENEMIES_ "BlackPhantomEnemies"
-#define _DS1_OVERHAUL_PREF_CM_GL_PHANTOMS_ "GravelordPhantoms"
-// Challenge mod settings:
-#define _DS1_OVERHAUL_PREF_CM_AGGRO_AI_EAR_DIST_  "AggressiveAIHearingDistance"
-#define _DS1_OVERHAUL_PREF_CM_AGGRO_AI_NOSE_DIST_ "AggressiveAISmellDistance"
-#define _DS1_OVERHAUL_PREF_CM_BP_ENEMY_DRAW_TYPE_ "BlackPhantomEnemyDrawType"
-// Debug:
-#define _DS1_OVERHAUL_PREF_MONITOR_BDT_ "MonitorBdtFiles"
-#define _DS1_OVERHAUL_PREF_MONITOR_BHD_ "MonitorBhdFiles"
-#define _DS1_OVERHAUL_PREF_MONITOR_SL2_ "MonitorSaveFile"
-#define _DS1_OVERHAUL_PREF_IO_OUT_FILTER_ "IoMonitorFilter"
-// Keybinds:
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_MOUSE_INPUT_ "ToggleMouseInput"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_CONSOLE_LOCK_CAM_ "ToggleConsoleLockCamera"
-#define _DS1_OVERHAUL_HOTKEY_BONFIRE_INPUT_FIX_ "BonfireInputFix"
-#define _DS1_OVERHAUL_HOTKEY_DESPAWN_GL_PHANTOMS_ "DespawnGravelordPhantoms"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_NODE_COUNT_ "ToggleNodeCount"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_DIM_LAVA_ "ToggleLavaBrightness"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_ARMOR_SFX_ "ToggleArmorSounds"
-#define _DS1_OVERHAUL_HOTKEY_SAVE_FILE_NEXT_ "NextSaveFile"
-#define _DS1_OVERHAUL_HOTKEY_SAVE_FILE_PREV_ "PrevSaveFile"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_COMPASS_RADIAL_ "ToggleHudCompassRadial"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_COMPASS_BAR_ "ToggleHudCompassBar"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_ELEVATION_METER_ "ToggleHudElevationMeter"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_NODE_GRAPH_ "ToggleHudNodeGraph"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_AC_BINOCS_TRIG_BLOCK_ "ToggleBinocsTriggerBlockAC"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_AC_DRAGON_TRIG_BLOCK_ "ToggleDragonTriggerBlockAC"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_CM_AGGRO_AI_ "ToggleChallengeAggressiveAI"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_CM_BP_ENEMIES_ "ToggleChallengeBlackPhantomEnemies"
-#define _DS1_OVERHAUL_HOTKEY_TOGGLE_CM_GL_PHANTOMS_ "ToggleChallengeGravelordPhantoms"
 
 #define _DS1_OVERHAUL_SETTINGS_STRING_BUFF_LEN_ 256  // Length of the string buffer used when obtaining user preference-related string data
 
-
-
 ////////////////////////////// MOD CONSTANTS //////////////////////////////
 #define _DS1_OVERHAUL_KEYPRESS_DELAY_ 200  // Sleep time (in milliseconds) after a keybind function is called, to avoid the keypress being detected multiple times
-
-
-
-//////////////////////////// OVERLAY MESSAGES ////////////////////////////
-#define _DS1_MOD_MSG_BONFIRE_INPUT_FIX_SUCCESS_ "SUCCESS: Bonfire input fix applied"
-#define _DS1_MOD_MSG_BONFIRE_INPUT_FIX_NO_INIT_FAIL_ "FAILED: Bonfire input fix was not applied (error reading memory)"
-#define _DS1_MOD_MSG_BONFIRE_INPUT_FIX_NO_CHAR_FAIL_ "FAILED: Bonfire input fix was not applied (no character loaded)"
-#define _DS1_MOD_MSG_BONFIRE_INPUT_FIX_NOT_AT_BONFIRE_FAIL_ "FAILED: Bonfire input fix was not applied (character is not sitting at a bonfire)"
-#define _DS1_MOD_MSG_BONFIRE_INPUT_FIX_AUTOMATIC_ "WARNING: Bonfire input glitch detected; applying input fix"
 
 
 #include <vector>
@@ -119,50 +47,17 @@ public:
     // Used in console messages to inform users that a message is being printed by the Overhaul mod
     static const std::string output_prefix;
 
-    // Set to true after inititalize_plugin() has executed
-    static bool initialized;
-
-    // Set to true after deferred_tasks() has executed (or this boolean can be set to true to cancel unfinished deferred tasks)
-    static bool deferred_tasks_complete;
-
-    // List of supported game versions
-    static std::vector<uint8_t> supported_game_versions;
-
-    // Enable/disable mouse input
-    static bool mouse_input;
-
-    // Lock camera when console is open
-    static bool console_lock_camera;
-
     // Determines whether to start in legacy mode (only applies fixes/anti-cheat/QoL changes, no gameplay modifications)
     static bool legacy_mode;
 
-    // Cheats on/off. If cheats are enabled, saving and multiplayer are disabled until the game is restarted
-    static bool cheats;
-
-    // Cheats warning message on/off
-    static bool cheats_warning;
-
     // Determines to disable the game's "Framerate insufficient for online play" disconnection
     static bool disable_low_fps_disconnect;
-
-    // Determines whether node count is displayed on the overlay text feed info header
-    static bool show_node_count;
-
-    // User preference setting; determines whether the brightness of lava visual effects should be lowered
-    static bool dim_lava_pref;
-
-    // User preference setting; determines whether multiplayer node graph HUD element will be enabled when network is loaded
-    static bool hud_node_graph_pref;
 
     // Custom game archive files to load instead of the vanilla game files
     static std::wstring custom_game_archive_path;
 
     // Custom game configuration file to load instead of the vanilla file
     static std::wstring custom_config_file_path;
-
-    // Registers all commands in ConsoleCommands.cpp for use with the in-game console
-    static void register_console_commands();
 
     // Get user-defined startup preferences from the settings file
     static void get_init_preferences();
--- OverhaulDLL/src/ModData.cpp
+++ OverhaulDLL/src/ModData.cpp
@@ -10,20 +10,10 @@
 #include "SP/preferences/user_setting.h"
 #include "SP/io/keybinds.h"
 #include "AntiCheat.h"
-//#include "Challenge/AggressiveAI.h"
-//#include "Challenge/BlackPhantomEnemies.h"
-//#include "Challenge/GravelordPhantoms.h"
 #include "AnimationEdits.h"
 #include "Files.h"
 
 #define _SP_DEFINE_VK_NAME_STRINGS_  // Must be defined to use Virtual-key code name strings from SP_IO_Strings.hpp (opt-in by default because it increases filesize by a few KB)
-
-#ifdef DS1_OVERHAUL_QOL_PREVIEW
-    #define DS1_OVERHAUL_LEGACY_MODE_DEFAULT_VAL true
-#else
-    #define DS1_OVERHAUL_LEGACY_MODE_DEFAULT_VAL false
-#endif // DS1_OVERHAUL_QOL_PREVIEW
-
 
 /*
     Initialize static variables:
@@ -33,38 +23,11 @@
 // Used in console messages to inform users that a message is being printed by the Overhaul mod
 const std::string Mod::output_prefix = "[Overhaul Mod] ";
 
-// Set to true after inititalize_plugin() has executed
-bool Mod::initialized = false;
-
-// Set to true after deferred_tasks() has executed (or can be set to true to cancel unfinished deferred tasks)
-bool Mod::deferred_tasks_complete = false;
-
-// Enable/disable mouse input
-bool Mod::mouse_input = true;
-
-// Lock camera when console is open
-bool Mod::console_lock_camera = true;
-
 // Determines whether to start in legacy mode (only applies fixes, no gameplay changes)
-bool Mod::legacy_mode = DS1_OVERHAUL_LEGACY_MODE_DEFAULT_VAL;
-
-// Cheats on/off. If cheats are enabled, saving and multiplayer are disabled until the game is restarted
-bool Mod::cheats = false;
-
-// Cheats warning message on/off
-bool Mod::cheats_warning = true;
+bool Mod::legacy_mode = false;
 
 // Determines to disable the game's "Low framerate detected" disconnection
 bool Mod::disable_low_fps_disconnect = false;
-
-// Determines whether node count is displayed on the overlay text feed info header
-bool Mod::show_node_count = true;
-
-// User preference setting; determines whether the brightness of lava visual effects should be lowered
-bool Mod::dim_lava_pref = false;
-
-// User preference setting; determines whether multiplayer node graph HUD element is enabled
-bool Mod::hud_node_graph_pref = false;
 
 // Custom game archive files to load instead of the vanilla game files
 std::wstring Mod::custom_game_archive_path;
@@ -86,11 +49,6 @@ void Mod::get_init_preferences()
     // Begin loading startup preferences
     global::cmd_out << (Mod::output_prefix + "Loading settings preferences...\n");
 
-    // Check if mouse input should be disabled
-    Mod::mouse_input = ((int)GetPrivateProfileInt(_DS1_OVERHAUL_INPUT_SECTION_, _DS1_OVERHAUL_PREF_MOUSE_INPUT_, (int)Mod::mouse_input, _DS1_OVERHAUL_SETTINGS_FILE_) != 0);
-    if (!Mod::mouse_input)
-        global::cmd_out << ("    Mouse input disabled.\n");
-
     // Check if legacy mode is enabled
     Mod::legacy_mode = ((int)GetPrivateProfileInt(_DS1_OVERHAUL_PREFS_SECTION_, _DS1_OVERHAUL_PREF_LEGACY_MODE_, (int)Mod::legacy_mode, _DS1_OVERHAUL_SETTINGS_FILE_) != 0);
     if (Mod::legacy_mode)
@@ -99,63 +57,7 @@ void Mod::get_init_preferences()
     // Check for custom game files
     Mod::get_custom_game_files();
 
-    // Check if camera should be locked when console is open
-    Mod::console_lock_camera = ((int)GetPrivateProfileInt(_DS1_OVERHAUL_INPUT_SECTION_, _DS1_OVERHAUL_PREF_CONSOLE_LOCK_CAM_, (int)Mod::console_lock_camera, _DS1_OVERHAUL_SETTINGS_FILE_) != 0);
-
-    Mod::cheats_warning = ((int)GetPrivateProfileInt(_DS1_OVERHAUL_PREFS_SECTION_, _DS1_OVERHAUL_PREF_CHEATS_WARNING_, (int)Mod::cheats_warning, _DS1_OVERHAUL_SETTINGS_FILE_) != 0);
-
     Mod::disable_low_fps_disconnect = ((int)GetPrivateProfileInt(_DS1_OVERHAUL_PREFS_SECTION_, _DS1_OVERHAUL_PREF_DISABLE_LOW_FPS_DISCONNECT_, (int)Mod::disable_low_fps_disconnect, _DS1_OVERHAUL_SETTINGS_FILE_) != 0);
-
-    // Display multiplayer node count in text feed info header
-    Mod::show_node_count = ((int)GetPrivateProfileInt(_DS1_OVERHAUL_PREFS_SECTION_, _DS1_OVERHAUL_PREF_SHOW_NODE_COUNT_, (int)Mod::show_node_count, _DS1_OVERHAUL_SETTINGS_FILE_) != 0);
-    msg = "    Display multiplayer node count = ";
-    if (Mod::show_node_count)
-        global::cmd_out << (msg.append("enabled\n"));
-    else
-        global::cmd_out << (msg.append("disabled\n"));
-
-
-    // Check whether to lower the brightness of lava effects
-    Mod::dim_lava_pref = ((int)GetPrivateProfileInt(_DS1_OVERHAUL_PREFS_SECTION_, _DS1_OVERHAUL_PREF_DIM_LAVA_, 0, _DS1_OVERHAUL_SETTINGS_FILE_) != 0);
-    if (Mod::dim_lava_pref)
-        global::cmd_out << ("    Lava visual effects will be dimmed");
-
-    // Check if additional HUD elements should be displayed
-    /*if ((int)GetPrivateProfileInt(_DS1_OVERHAUL_HUD_SECTION_, _DS1_OVERHAUL_PREF_COMPASS_RADIAL_, 0, _DS1_OVERHAUL_SETTINGS_FILE_) != 0) {
-    Hud::set_show_compass_radial(true);
-    global::cmd_out << ("    HUD: Radial compass enabled");
-    }*/
-
-    /*if ((int)GetPrivateProfileInt(_DS1_OVERHAUL_HUD_SECTION_, _DS1_OVERHAUL_PREF_COMPASS_BAR_, 0, _DS1_OVERHAUL_SETTINGS_FILE_) != 0) {
-    Hud::set_show_compass_bar(true);
-    global::cmd_out << ("    HUD: Bar compass enabled");
-    }*/
-
-    /*if ((int)GetPrivateProfileInt(_DS1_OVERHAUL_HUD_SECTION_, _DS1_OVERHAUL_PREF_ELEVATION_, 0, _DS1_OVERHAUL_SETTINGS_FILE_) != 0) {
-    Hud::set_show_elevation_meter(true);
-    global::cmd_out << ("    HUD: Elevation meter enabled");
-    }*/
-
-    /*if ((int)GetPrivateProfileInt(_DS1_OVERHAUL_HUD_SECTION_, _DS1_OVERHAUL_PREF_NODE_GRAPH_, Hud::get_show_node_graph(), _DS1_OVERHAUL_SETTINGS_FILE_) != 0) {
-    Hud::set_show_node_graph(true, false);
-    global::cmd_out << ("    HUD: Multiplayer node graph enabled");
-    }*/
-
-    // Check if challenge mods should be enabled
-#if 0
-    if ((int)GetPrivateProfileInt(_DS1_OVERHAUL_CHALLENGE_SECTION_, _DS1_OVERHAUL_PREF_CM_AGGRO_AI_, Challenge::AggressiveAi::active(), _DS1_OVERHAUL_SETTINGS_FILE_) != 0) {
-        global::cmd_out << ("    Challenge: Aggressive AI will be enabled");
-    }
-    if ((int)GetPrivateProfileInt(_DS1_OVERHAUL_CHALLENGE_SECTION_, _DS1_OVERHAUL_PREF_CM_GL_PHANTOMS_, Challenge::GravelordPhantoms::active, _DS1_OVERHAUL_SETTINGS_FILE_) != 0) {
-        global::cmd_out << ("    Challenge: Auto-spawning Gravelord Phantoms will be enabled");
-    }
-    if ((int)GetPrivateProfileInt(_DS1_OVERHAUL_CHALLENGE_SECTION_, _DS1_OVERHAUL_PREF_CM_BP_ENEMIES_, Challenge::BlackPhantomEnemies::active, _DS1_OVERHAUL_SETTINGS_FILE_) != 0) {
-        global::cmd_out << ("    Challenge: Black Phantom Enemies will be enabled");
-    }
-    Challenge::AggressiveAi::ear_distance = (uint16_t)GetPrivateProfileInt(_DS1_OVERHAUL_CHALLENGE_SETTINGS_SECTION_, _DS1_OVERHAUL_PREF_CM_AGGRO_AI_EAR_DIST_, Challenge::AggressiveAi::DEFAULT_EAR_DISTANCE, _DS1_OVERHAUL_SETTINGS_FILE_);
-    Challenge::AggressiveAi::nose_distance = (uint16_t)GetPrivateProfileInt(_DS1_OVERHAUL_CHALLENGE_SETTINGS_SECTION_, _DS1_OVERHAUL_PREF_CM_AGGRO_AI_NOSE_DIST_, Challenge::AggressiveAi::DEFAULT_NOSE_DISTANCE, _DS1_OVERHAUL_SETTINGS_FILE_);
-    Challenge::BlackPhantomEnemies::DRAW_TYPE = (uint8_t)GetPrivateProfileInt(_DS1_OVERHAUL_CHALLENGE_SETTINGS_SECTION_, _DS1_OVERHAUL_PREF_CM_BP_ENEMY_DRAW_TYPE_, Challenge::BlackPhantomEnemies::DRAW_TYPE_DEFAULT, _DS1_OVERHAUL_SETTINGS_FILE_);
-#endif
 }
 
 // Get user-defined keybinds from the settings file
@@ -164,56 +66,10 @@ void Mod::get_user_keybinds()
     // Begin loading keybinds
     global::cmd_out << (Mod::output_prefix + "Loading keybinds...\n");
 
-
+    // Load keybinds here
     // Toggle mouse input keybind
     //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_MOUSE_INPUT_, kf_toggle_mouse_input);
-
-    // Toggle camera lock when console is open
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_CONSOLE_LOCK_CAM_, kf_toggle_console_lock_cam);
-
-    // Bonfire input fix keybind
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_BONFIRE_INPUT_FIX_, kf_fix_bonfire_input);
-
-    // Toggle multiplayer node count in text feed info header
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_NODE_COUNT_, kf_toggle_node_count);
-
-
-    // Toggle dimmed lava visual effects
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_DIM_LAVA_, kf_toggle_dim_lava);
-
-#ifndef DS1_OVERHAUL_QOL_PREVIEW
-    // Toggle armor sound effects
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_ARMOR_SFX_, kf_toggle_armor_sfx);
-#endif // DS1_OVERHAUL_QOL_PREVIEW
-
-    // De-spawn existing Gravelord phantoms
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_DESPAWN_GL_PHANTOMS_, kf_gravelord_phantoms_despawn);
-
-    // Next/previous save file
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_SAVE_FILE_NEXT_, kf_save_file_next);
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_SAVE_FILE_PREV_, kf_save_file_prev);
-
-    // Toggle additional HUD elements
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_COMPASS_RADIAL_, kf_toggle_hud_compass_radial);
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_COMPASS_BAR_, kf_toggle_hud_compass_bar);
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_ELEVATION_METER_, kf_toggle_hud_elevation_meter);
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_HUD_NODE_GRAPH_, kf_toggle_hud_node_graph);
-    
-    // Toggle anti-cheats (not all are togglable)
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_AC_BINOCS_TRIG_BLOCK_, kf_toggle_ac_binocs_trigger_block);
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_AC_DRAGON_TRIG_BLOCK_, kf_toggle_ac_dragon_trigger_block);
-
-    // Toggle challenge mods
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_CM_AGGRO_AI_, kf_toggle_cm_aggressive_ai);
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_CM_BP_ENEMIES_, kf_toggle_cm_bp_enemies);
-    //get_single_user_keybind(_DS1_OVERHAUL_HOTKEY_TOGGLE_CM_GL_PHANTOMS_, kf_toggle_cm_gravelord_phantoms);
-
-    // @TODO Load additional keybinds here
-
-
 }
-
-
 
 // Helper function for get_user_keybinds() that loads the specified keybind from the config file and binds it to the specified function
 void Mod::get_single_user_keybind(const char *keybind_name, int(*function)())
