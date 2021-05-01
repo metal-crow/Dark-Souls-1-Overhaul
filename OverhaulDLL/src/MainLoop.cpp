d17c6231989b9f0dad644ce8d5b9e8291e83d537
metal-crow <frankcrisanti@aol.com>
2021-05-01 03:02:49 -0400

    fix bug with iterating over callbacks after adding ones

--- OverhaulDLL/src/MainLoop.cpp
+++ OverhaulDLL/src/MainLoop.cpp
@@ -3,6 +3,7 @@
 #include "SP/memory/injection/asm/x64.h"
 #include <vector>
 #include <tuple>
+#include <mutex>
 
 extern "C" {
     uint64_t main_game_loop_injection_return;
@@ -21,14 +22,29 @@ void MainLoop::start()
 //contains the function ptr and the data
 static std::vector<std::tuple<MainLoopCallback, void*, std::string>> callbacks;
 
+//adding an item from a std::vector invalidates existing iterators, so we need to only add new items when we're not iterating
+static std::vector<std::tuple<MainLoopCallback, void*, std::string>> callbacks_queue;
+std::mutex callbacks_queue_mtx;
+
 //given the function and the data arg for it, save it so we call it every frame until it returns false
 void MainLoop::setup_mainloop_callback(MainLoopCallback func, void* data, std::string name)
 {
-    callbacks.push_back(std::make_tuple(func, data, name));
+    callbacks_queue_mtx.lock();
+    callbacks_queue.push_back(std::make_tuple(func, data, name));
+    callbacks_queue_mtx.unlock();
 }
 
 void main_game_loop_injection_helper()
 {
+    //move any items from the queue to our real list
+    callbacks_queue_mtx.lock();
+    for (int i = 0; i < callbacks_queue.size(); i++)
+    {
+        callbacks.push_back(callbacks_queue[i]);
+    }
+    callbacks_queue.clear();
+    callbacks_queue_mtx.unlock();
+
     //run through all the callbacks
     auto iter = callbacks.begin();
     while (iter != callbacks.end())
@@ -41,6 +57,7 @@ void main_game_loop_injection_helper()
         //when the function returns false, remove from our callbacks
         if (result == false)
         {
+            //removing an item invalidates existing iterators, so we need to get a new one
             iter = callbacks.erase(iter);
         }
         else
