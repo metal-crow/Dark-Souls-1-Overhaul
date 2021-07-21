a867e41055a487a80554bf4d0450e2fc3bbd3067
metal-crow <frankcrisanti@aol.com>
2021-07-21 18:53:57 -0400

    add version string printing

--- OverhaulDLL/include/DarkSoulsOverhaulMod.h
+++ OverhaulDLL/include/DarkSoulsOverhaulMod.h
@@ -28,7 +28,8 @@
 // Preprocessor definition utilized by the Dark Souls Game Files C++ library (DsGameFiles.lib)
 #define __DS1_OVERHAUL_MOD
 
-#define DS1_OVERHAUL_TXT_INTRO "\n+-------------------------------+\n|      Dark Souls Overhaul      |\n+-------------------------------+"
+#define VERSION_STRING  "Alpha7"
+#define DS1_OVERHAUL_TXT_INTRO "\n+------------------------------------------------------+\n|      Dark Souls Overhaul   ["VERSION_STRING"]                  |\n+------------------------------------------------------+"
 
 extern HMODULE d3d11_module;
 
