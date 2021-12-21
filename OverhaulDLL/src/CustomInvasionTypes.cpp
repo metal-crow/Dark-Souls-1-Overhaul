074cdef29bfefe2c8ee467f33c651c4bba962d4e
metal-crow <frankcrisanti@aol.com>
2021-12-20 20:45:44 -0500

    fix bug with how the game handles type17 server packets, and how we should modify them.

--- OverhaulDLL/src/CustomInvasionTypes.cpp
+++ OverhaulDLL/src/CustomInvasionTypes.cpp
@@ -17,6 +17,7 @@ void CustomInvasionTypes::start()
     sp::mem::code::x64::inject_jmp_14b(write_address, &Send_Type17_GeneralRequestTask_injection_return, 1, &Send_Type17_GeneralRequestTask_injection);
 }
 
+static uint32_t last_frame = 0;
 static size_t current_mpregionid_offset = 0;
 static bool UsingAllAreasInvadeOrb = false;
 static uint32_t current_soullevel_offset = 0;
@@ -53,7 +54,15 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
     {
         //use the current area id in the list
         *(uint32_t*)(RequestGetBreakInTargetList_Data) = MultiPlayerRegionIDs[current_mpregionid_offset];
-        current_mpregionid_offset++;
+
+        //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
+        //so to be safe, we don't change from the 1st request
+        //(the two requests are not on the same frame, but are about 15 frames apart)
+        if (last_frame+30 > Game::get_frame_count())
+        {
+            current_mpregionid_offset++;
+        }
+
         if (current_mpregionid_offset >= (sizeof(MultiPlayerRegionIDs) / sizeof(MultiPlayerRegionIDs[0])))
         {
             current_mpregionid_offset = 0;
@@ -73,18 +82,29 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
         //Add an offset to the SL we tell the server, so it returns a new range of connection results
         uint32_t pc_sl = *(uint32_t*)(RequestGetBreakInTargetList_Data + 28);
         *(uint32_t*)(RequestGetBreakInTargetList_Data + 28) += current_soullevel_offset;
-        current_soullevel_offset += (20 + (uint32_t)(0.1f*pc_sl)); //increase by the search's upper bound: 20+0.1*SL
+        uint32_t pc_searched_sl = pc_sl + current_soullevel_offset;
+
+        //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
+        //so to be safe, we don't change from the 1st request
+        //(the two requests are not on the same frame, but are about 15 frames apart)
+        if (last_frame+30 > Game::get_frame_count())
+        {
+            current_soullevel_offset += 20 + (uint32_t)(0.1f*(pc_searched_sl)); //increase by the search's upper bound: 20+0.1*SL
+        }
+
         if (pc_sl + current_soullevel_offset > 713)
         {
             current_soullevel_offset = 0;
         }
 
         //set the timer to be closer to the refresh time (30 seconds)
-        Game::set_invasion_refresh_timer(15.0f);
+        Game::set_invasion_refresh_timer(25.0f);
     }
     else
     {
         //reset the offset
         current_soullevel_offset = 0;
     }
+
+    last_frame = Game::get_frame_count();
 }
