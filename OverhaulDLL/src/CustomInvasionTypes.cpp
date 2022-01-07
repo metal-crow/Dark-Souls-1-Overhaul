5abce5ebfe731b1e5003dad184526f2f1509f00e
metal-crow <frankcrisanti@aol.com>
2022-01-06 19:27:11 -0500

    increase the speed of searching, and fix a bug thta appeared because of that

--- OverhaulDLL/src/CustomInvasionTypes.cpp
+++ OverhaulDLL/src/CustomInvasionTypes.cpp
@@ -17,7 +17,7 @@ void CustomInvasionTypes::start()
     sp::mem::code::x64::inject_jmp_14b(write_address, &Send_Type17_GeneralRequestTask_injection_return, 1, &Send_Type17_GeneralRequestTask_injection);
 }
 
-static uint32_t last_frame = 0;
+static uint32_t last_send_count = 0;
 static size_t current_mpregionid_offset = 0;
 static bool UsingAllAreasInvadeOrb = false;
 static uint32_t current_soullevel_offset = 0;
@@ -37,17 +37,20 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
     {
         UsingAllAreasInvadeOrb = true;
         UsingInfiniteUpwardsInvadeOrb = false;
+        last_send_count = 0;
     }
     else if (Game::player_has_speffect((uint64_t)(playerins_o.value()), { CustomInvasionTypes::InfiniteUpwardsInvadingOrbSpEffect }))
     {
         UsingAllAreasInvadeOrb = false;
         UsingInfiniteUpwardsInvadeOrb = true;
+        last_send_count = 0;
     }
     //If the player is doing any other normal multiplayer types, don't do this custom code
     else if (Game::player_has_speffect((uint64_t)(playerins_o.value()), { 4, 10, 11, 16, 26, 27, 15 }))
     {
         UsingAllAreasInvadeOrb = false;
         UsingInfiniteUpwardsInvadeOrb = false;
+        last_send_count = 0;
     }
 
     if (UsingAllAreasInvadeOrb)
@@ -55,10 +58,17 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
         //use the current area id in the list
         *(uint32_t*)(RequestGetBreakInTargetList_Data) = MultiPlayerRegionIDs[current_mpregionid_offset];
 
+        ConsoleWrite("Searching orb: area %d", MultiPlayerRegionIDs[current_mpregionid_offset]);
+        last_send_count++;
+        if (last_send_count >= 2)
+        {
+            last_send_count = 0;
+        }
+
         //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
         //so to be safe, we don't change from the 1st request
-        //(the two requests are not on the same frame, but are about 15 frames apart)
-        if (last_frame+30 > Game::get_frame_count())
+        //(the two requests are not on the same frame)
+        if (last_send_count == 0)
         {
             current_mpregionid_offset++;
         }
@@ -84,10 +94,17 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
         *(uint32_t*)(RequestGetBreakInTargetList_Data + 28) += current_soullevel_offset;
         uint32_t pc_searched_sl = pc_sl + current_soullevel_offset;
 
+        ConsoleWrite("Unbound orb: SL %d", pc_searched_sl);
+        last_send_count++;
+        if (last_send_count >= 2)
+        {
+            last_send_count = 0;
+        }
+
         //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
         //so to be safe, we don't change from the 1st request
-        //(the two requests are not on the same frame, but are about 15 frames apart)
-        if (last_frame+30 > Game::get_frame_count())
+        //(the two requests are not on the same frame)
+        if (last_send_count == 0)
         {
             current_soullevel_offset += 20 + (uint32_t)(0.1f*(pc_searched_sl)); //increase by the search's upper bound: 20+0.1*SL
         }
@@ -105,6 +122,4 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
         //reset the offset
         current_soullevel_offset = 0;
     }
-
-    last_frame = Game::get_frame_count();
 }
