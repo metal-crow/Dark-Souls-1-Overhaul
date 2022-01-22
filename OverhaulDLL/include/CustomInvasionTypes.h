a0954be5f8be4b3b3df5fb6f5c8df3b32c8dd8f9
metal-crow <frankcrisanti@aol.com>
2022-01-22 17:30:13 -0500

    allow users to set what areas they want to search in invasions

--- OverhaulDLL/include/CustomInvasionTypes.h
+++ OverhaulDLL/include/CustomInvasionTypes.h
@@ -3,73 +3,13 @@
 #define CustomInvasionTypes_H
 
 #include <cstdint>
-#include <tuple>
-
-//These only need to include 1 area id from each set that match all but the last digit
-//i.e 100000 can invade all areas matching 10000X
-//http://darksouls.wikidot.com/invasion-spawn-locations
-static const std::tuple<uint32_t, const wchar_t*> MultiPlayerRegionIDs[] =
-{
-    //Depths
-    {100000, L"The Depths"},
-    {100010, L"Lower Depths"},
-    //Undead parish
-    {101200, L"Undead Parish Bridge"},
-    {101210, L"Upper Undead Parish"},
-    //Undead burg
-    {101000, L"Undead Burg Start"},
-    {101010, L"Undead Burg"},
-    {101100, L"Lower Undead Burg"},
-    //Painted world
-    {110000, L"Painted World"},
-    {110010, L"Painted World"},
-    //Darkroot garden / basin
-    {120000, L"Darkroot Garden and Basin"},
-    {120010, L"Darkroot Garden"},
-    {120100, L"Darkroot Garden Forest"},
-    //Artorias of the Abyss areas
-    {121000, L"Sanctuary Garden"},
-    {121100, L"Royal Wood"},
-    {121200, L"Oolacile Township"},
-    {121210, L"Chasm of the Abyss"},
-    //the catacombs
-    {130000, L"The Catacombs Start"},
-    {130010, L"The Catacombs"},
-    //tomb of the giants
-    {131000, L"Tomb of the Giants Upper"},
-    {131010, L"Tomb of the Giants Lower"},
-    //blighttown
-    {140000, L"Upper Blighttown"},
-    {140010, L"Blighttown Swamp"},
-    //demon ruins / lost izaleth
-    {141000, L"Ceaseless Discharge Bridge"},
-    {141100, L"Demon Ruins"},
-    {141200, L"Centipede Demon Hallway"},
-    {141300, L"Lost Izalith"},
-    //Sens fortress
-    {150000, L"Sen's Fortress"},
-    {150010, L"Sen's Fortress"},
-    {150020, L"Sen's Fortress Roof"},
-    //Anor Londo
-    {151000, L"Anor Londo Start"},
-    {151010, L"Anor Londo"},
-    {151020, L"Anor Londo Interior"},
-    //New Londo / vally of drakes
-    {160100, L"New Londo Start"},
-    {160110, L"New Londo"},
-    {160120, L"New Londo Ground"},
-    //Dukes archive / crystal caves
-    {170000, L"Duke's Archives"},
-    {170010, L"Outside Duke's Archives"},
-    //Kiln
-    {180000, L"Kiln of the First Flame"},
-};
 
 //This handles WexDust invasions and Infinite Upwards SL search invasions
 class CustomInvasionTypes
 {
 public:
     static void start();
+    static void ReadSearchFile();
 
     static const uint32_t AllAreasInvadingOrbSpEffect_Red = 18;
     static const uint32_t AllAreasInvadingOrbSpEffect_Blue = 91;
--- OverhaulDLL/src/CustomInvasionTypes.cpp
+++ OverhaulDLL/src/CustomInvasionTypes.cpp
@@ -2,6 +2,9 @@
 #include "DarkSoulsOverhaulMod.h"
 #include "SP/memory/injection/asm/x64.h"
 #include "MainLoop.h"
+#include <tuple>
+#include <vector>
+#include <string>
 
 extern "C" {
     uint64_t Send_Type17_GeneralRequestTask_injection_return;
@@ -15,6 +18,59 @@ void CustomInvasionTypes::start()
 
     uint8_t *write_address = (uint8_t*)(CustomInvasionTypes::Send_Type17_GeneralRequestTask_offset + Game::ds1_base);
     sp::mem::code::x64::inject_jmp_14b(write_address, &Send_Type17_GeneralRequestTask_injection_return, 1, &Send_Type17_GeneralRequestTask_injection);
+}
+
+//These only need to include 1 area id from each set that match all but the last digit
+//i.e 100000 can invade all areas matching 10000X
+//http://darksouls.wikidot.com/invasion-spawn-locations
+static std::vector<std::tuple<uint32_t, std::wstring>> MultiPlayerRegionIDs;
+
+//read the data from the text file that specifics the areas to search, and their names
+//run when a character is loaded/reloaded from the save file
+void CustomInvasionTypes::ReadSearchFile()
+{
+    FILE* fp = NULL;
+    char line[80];
+    uint32_t mpid;
+    char area_name[80];
+
+    fopen_s(&fp, "./searchlist.txt", "r");
+    if (fp == NULL)
+    {
+        ConsoleWrite("WARNING: Searchlist.txt not found. Unable to use searching eye orb.");
+        return;
+    }
+
+    MultiPlayerRegionIDs.clear();
+
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        //ignore comments
+        if (line[0] != '#')
+        {
+            int matched = sscanf_s(line, "%lu:%79[^\r\n].*", &mpid, area_name, (unsigned)_countof(area_name));
+            if (matched != 2)
+            {
+                ConsoleWrite("WARNING: Searchlist.txt has malformed line \"%s\"", line);
+            }
+            else
+            {
+                //convert area name to wstring
+                const size_t size = std::strlen(area_name);
+                std::wstring wstr;
+                wstr.resize(size);
+                mbstowcs_s(NULL, &wstr[0], wstr.capacity(), area_name, _TRUNCATE);
+
+                //insert this entry
+                auto entry = std::make_tuple(mpid, wstr);
+                MultiPlayerRegionIDs.push_back(entry); //this is fine since a vector makes a copy of the string
+            }
+        }
+    }
+
+    fclose(fp);
+
+    ConsoleWrite("Invasion search areas reloaded");
 }
 
 enum InvasionOrb
@@ -71,34 +127,36 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
     if (current_InvasionOrb == InvasionOrb::AllAreas)
     {
         //use the current area id in the list
-        *(uint32_t*)(RequestGetBreakInTargetList_Data) = std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]);
-
-        swprintf(banner_msg, sizeof(banner_msg)/sizeof(wchar_t), L"Searching area: %s", std::get<1>(MultiPlayerRegionIDs[current_mpregionid_offset]));
-        Game::show_banner_message(banner_msg);
-
-        ConsoleWrite("Searching orb: area %d", std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]));
-        last_send_count++;
-        if (last_send_count >= 2)
+        if (MultiPlayerRegionIDs.size() > 0)
         {
-            last_send_count = 0;
-        }
+            *(uint32_t*)(RequestGetBreakInTargetList_Data) = std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]);
 
-        //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
-        //so to be safe, we don't change from the 1st request
-        //(the two requests are not on the same frame)
-        if (last_send_count == 0)
-        {
-            current_mpregionid_offset++;
-        }
+            swprintf(banner_msg, sizeof(banner_msg) / sizeof(wchar_t), L"Searching area: %s", std::get<1>(MultiPlayerRegionIDs[current_mpregionid_offset]).c_str());
+            Game::show_banner_message(banner_msg);
 
-        if (current_mpregionid_offset >= (sizeof(MultiPlayerRegionIDs) / sizeof(MultiPlayerRegionIDs[0])))
-        {
-            current_mpregionid_offset = 0;
-        }
+            ConsoleWrite("Searching orb: area %d", std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]));
+            last_send_count++;
+            if (last_send_count >= 2)
+            {
+                last_send_count = 0;
+            }
 
-        //set the timer to be closer to the refresh time (30 seconds)
-        Game::set_invasion_refresh_timer(26.0f);
-        Game::set_blue_invasion_refresh_timer(26.0f);
+            //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
+            //so to be safe, we don't change from the 1st request
+            //(the two requests are not on the same frame)
+            if (last_send_count == 0)
+            {
+                current_mpregionid_offset++;
+                if (current_mpregionid_offset >= MultiPlayerRegionIDs.size())
+                {
+                    current_mpregionid_offset = 0;
+                }
+            }
+
+            //set the timer to be closer to the refresh time (30 seconds)
+            Game::set_invasion_refresh_timer(26.0f);
+            Game::set_blue_invasion_refresh_timer(26.0f);
+        }
     }
 
     if (current_InvasionOrb == InvasionOrb::InfiniteUp)
@@ -142,52 +200,55 @@ void Send_Type17_GeneralRequestTask_injection_helper(uint64_t RequestGetBreakInT
 
     if (current_InvasionOrb == InvasionOrb::AllAreasAndInfiniteUp)
     {
-        *(uint32_t*)(RequestGetBreakInTargetList_Data) = std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]);
-
-        uint32_t pc_sl = *(uint32_t*)(RequestGetBreakInTargetList_Data + 28);
-        *(uint32_t*)(RequestGetBreakInTargetList_Data + 28) += current_soullevel_offset;
-        uint32_t pc_searched_sl = pc_sl + current_soullevel_offset;
-
-        swprintf(banner_msg, sizeof(banner_msg) / sizeof(wchar_t), L"Searching area: %s at SL: %d", std::get<1>(MultiPlayerRegionIDs[current_mpregionid_offset]), pc_searched_sl);
-        Game::show_banner_message(banner_msg);
-
-        ConsoleWrite("Twin eye orb: area %d SL %d", std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]), pc_searched_sl);
-        last_send_count++;
-        if (last_send_count >= 2)
+        if (MultiPlayerRegionIDs.size() > 0)
         {
-            last_send_count = 0;
+            *(uint32_t*)(RequestGetBreakInTargetList_Data) = std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]);
+
+            uint32_t pc_sl = *(uint32_t*)(RequestGetBreakInTargetList_Data + 28);
+            *(uint32_t*)(RequestGetBreakInTargetList_Data + 28) += current_soullevel_offset;
+            uint32_t pc_searched_sl = pc_sl + current_soullevel_offset;
+
+            swprintf(banner_msg, sizeof(banner_msg) / sizeof(wchar_t), L"Searching area: %s at SL: %d", std::get<1>(MultiPlayerRegionIDs[current_mpregionid_offset]).c_str(), pc_searched_sl);
+            Game::show_banner_message(banner_msg);
+
+            ConsoleWrite("Twin eye orb: area %d SL %d", std::get<0>(MultiPlayerRegionIDs[current_mpregionid_offset]), pc_searched_sl);
+            last_send_count++;
+            if (last_send_count >= 2)
+            {
+                last_send_count = 0;
+            }
+
+            //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
+            //so to be safe, we don't change from the 1st request
+            //(the two requests are not on the same frame)
+            if (last_send_count == 0)
+            {
+                //increase the SL offset first
+                current_soullevel_offset += (uint32_t)(1.22222*pc_searched_sl + 22.2222) - pc_searched_sl; //increase by the search's upper and lower bounds
+                //clamp the last search value to 713 if needed
+                if (pc_sl + current_soullevel_offset > 713)
+                {
+                    current_soullevel_offset = 713 - pc_sl;
+                }
+                if (pc_searched_sl >= 713)
+                {
+                    current_soullevel_offset = 0;
+                }
+
+                //once the SL offset has looped around, go to next area
+                if (current_soullevel_offset == 0)
+                {
+                    current_mpregionid_offset++;
+                }
+                if (current_mpregionid_offset >= MultiPlayerRegionIDs.size())
+                {
+                    current_mpregionid_offset = 0;
+                }
+            }
+
+            //set the timer to be closer to the refresh time (30 seconds)
+            Game::set_invasion_refresh_timer(26.0f);
+            Game::set_blue_invasion_refresh_timer(26.0f);
         }
-
-        //for some reason, the game calls this function twice for each request. Not sure if one is bad or good or what
-        //so to be safe, we don't change from the 1st request
-        //(the two requests are not on the same frame)
-        if (last_send_count == 0)
-        {
-            //increase the SL offset first
-            current_soullevel_offset += (uint32_t)(1.22222*pc_searched_sl + 22.2222) - pc_searched_sl; //increase by the search's upper and lower bounds
-            //clamp the last search value to 713 if needed
-            if (pc_sl + current_soullevel_offset > 713)
-            {
-                current_soullevel_offset = 713 - pc_sl;
-            }
-            if (pc_searched_sl >= 713)
-            {
-                current_soullevel_offset = 0;
-            }
-
-            //once the SL offset has looped around, go to next area
-            if (current_soullevel_offset == 0)
-            {
-                current_mpregionid_offset++;
-            }
-            if (current_mpregionid_offset >= (sizeof(MultiPlayerRegionIDs) / sizeof(MultiPlayerRegionIDs[0])))
-            {
-                current_mpregionid_offset = 0;
-            }
-        }
-
-        //set the timer to be closer to the refresh time (30 seconds)
-        Game::set_invasion_refresh_timer(26.0f);
-        Game::set_blue_invasion_refresh_timer(26.0f);
     }
 }
