9a648cfa66bf06f9c211a3620dbe7c165ace5c5e
metal-crow <frankcrisanti@aol.com>
2022-01-23 16:59:45 -0500

    version str bump

--- OverhaulDLL/include/DarkSoulsOverhaulMod.h
+++ OverhaulDLL/include/DarkSoulsOverhaulMod.h
@@ -29,7 +29,7 @@
 #define __DS1_OVERHAUL_MOD
 
 static const uint64_t VERSION_RAW = 2022012300;
-#define VERSION_STRING  "Beta10"
+#define VERSION_STRING  "Beta11"
 #define DS1_OVERHAUL_TXT_INTRO "\n+------------------------------------------------------+\n|      Dark Souls Overhaul   ["VERSION_STRING"]                 |\n+------------------------------------------------------+"
 
 extern HMODULE d3d11_module;
