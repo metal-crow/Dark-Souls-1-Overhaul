a0b2111c87e7a0e1bb11fdff1065074d76adb150
metal-crow <frankcrisanti@aol.com>
2022-06-26 00:38:22 -0400

    more bulletman funcs

--- OverhaulDLL/include/BulletManStructFunctions.h
+++ OverhaulDLL/include/BulletManStructFunctions.h
@@ -9,5 +9,34 @@ void copy_BulletMan(BulletMan* to, BulletMan* from, bool to_game);
 BulletMan* init_BulletMan();
 void free_BulletMan(BulletMan* to);
 
+void copy_BulletIns(BulletIns* to, BulletIns* from, bool to_game);
+BulletIns* init_BulletIns();
+void free_BulletIns(BulletIns* to, bool freeself);
+
+void copy_BulletIns_FollowupBullet(BulletIns_FollowupBullet* to, BulletIns_FollowupBullet* from, bool to_game);
+
+void copy_BulletIns_Field0x90_Field0x1a0(BulletIns_Field0x90_Field0x1a0* to, BulletIns_Field0x90_Field0x1a0* from, bool to_game);
+
+void copy_BulletTargetingSystemOwner(BulletTargetingSystemOwner* to, BulletTargetingSystemOwner* from, bool to_game);
+
+void copy_TargetingSystemBase(TargetingSystemBase* to, TargetingSystemBase* from, bool to_game);
+
+void copy_BulletState(BulletState* to, BulletState* from, bool to_game);
+
+void copy_BulletFlyState(BulletFlyState* to, BulletFlyState* from, bool to_game);
+
+void copy_BulletParamInfo(BulletParamInfo* to, BulletParamInfo* from, bool to_game);
+
+void copy_BulletMan_Field0x20(BulletMan_Field0x20* to, BulletMan_Field0x20* from, bool to_game);
+BulletMan_Field0x20* init_BulletMan_Field0x20();
+void free_BulletMan_Field0x20(BulletMan_Field0x20* to, bool freeself);
+
+void copy_BulletMan_Field0x40(BulletMan_Field0x40* to, BulletMan_Field0x40* from, bool to_game);
+BulletMan_Field0x40* init_BulletMan_Field0x40();
+void free_BulletMan_Field0x40(BulletMan_Field0x40* to, bool freeself);
+
+void copy_BulletMan_field0x78Elem(BulletMan_field0x78Elem* to, BulletMan_field0x78Elem* from, bool to_game);
+BulletMan_field0x78Elem* init_BulletMan_field0x78Elem();
+void free_BulletMan_field0x78Elem(BulletMan_field0x78Elem* to);
 
 #endif
--- OverhaulDLL/src/BulletManStructFunctions.cpp
+++ OverhaulDLL/src/BulletManStructFunctions.cpp
@@ -85,18 +85,177 @@ void free_BulletMan(BulletMan* to)
     {
         free_BulletIns(&to->bulletins_arry[i], false);
     }
+    free(to->bulletins_arry);
     for (size_t i = 0; i < 40; i++)
     {
         free_BulletMan_Field0x20(&to->field0x20[i], false);
     }
+    free(to->field0x20);
     for (size_t i = 0; i < 4; i++)
     {
         free_BulletMan_Field0x40(&to->field0x40[i], false);
     }
+    free(to->field0x40);
     for (size_t i = 0; i < 3; i++)
     {
         free_BulletMan_field0x78Elem(to->field0x78[i]);
     }
     free(to->field0x78);
 
+    free(to);
+}
+
+void copy_BulletIns(BulletIns* to, BulletIns* from, bool to_game)
+{
+    memcpy(to->data_0, from->data_0, sizeof(to->data_0));
+    copy_BulletIns_FollowupBullet(&to->FollowupBullet, &from->FollowupBullet, to_game);
+    to->data_1 = from->data_1;
+    copy_BulletIns_Field0x90_Field0x1a0(&to->owner, &from->owner, to_game);
+    memcpy(to->data_2, from->data_2, sizeof(to->data_2));
+    copy_BulletTargetingSystemOwner(&to->bulletTargetingSystemOwner, &from->bulletTargetingSystemOwner, to_game);
+    copy_TargetingSystemBase(&to->targetingSystemBase, &from->targetingSystemBase, to_game);
+    memcpy(to->data_3, from->data_3, sizeof(to->data_3));
+    copy_BulletState(&to->bulletState, &from->bulletState, to_game);
+    to->data_4 = from->data_4;
+    copy_BulletFlyState(&to->bulletFlyState, &from->bulletFlyState, to_game);
+    copy_BulletState(&to->bulletExplosionState, &from->bulletExplosionState, to_game);
+
+    if (to->previous_bullet_in_use == NULL || from->previous_bullet_in_use == NULL)
+    {
+        FATALERROR("Unable to recursivly copy previous_bullet_in_use. Out of room.");
+    }
+    copy_BulletIns(to->previous_bullet_in_use, from->previous_bullet_in_use, to_game);
+
+    to->data_5 = from->data_5;
+}
+
+BulletIns* init_BulletIns()
+{
+    BulletIns* local_BulletIns = (BulletIns*)malloc_(sizeof(BulletIns));
+
+    //none of the substruct have pointers, so can save some code here
+
+    //1 recursion level deep
+    local_BulletIns->previous_bullet_in_use = (BulletIns*)malloc_(sizeof(BulletIns));
+    
+    return local_BulletIns;
+}
+
+void free_BulletIns(BulletIns* to, bool freeself)
+{
+    free(to->previous_bullet_in_use);
+
+    if (freeself)
+    {
+        free(to);
+    }
+}
+
+void copy_BulletIns_FollowupBullet(BulletIns_FollowupBullet* to, BulletIns_FollowupBullet* from, bool to_game)
+{
+    return;
+}
+
+void copy_BulletIns_Field0x90_Field0x1a0(BulletIns_Field0x90_Field0x1a0* to, BulletIns_Field0x90_Field0x1a0* from, bool to_game)
+{
+    memcpy(to->data_0, from->data_0, sizeof(to->data_0));
+    memcpy(to->data_1, from->data_1, sizeof(to->data_1));
+    memcpy(to->data_2, from->data_2, sizeof(to->data_2));
+    memcpy(to->data_3, from->data_3, sizeof(to->data_3));
+    memcpy(to->data_4, from->data_4, sizeof(to->data_4));
+    memcpy(to->data_5, from->data_5, sizeof(to->data_5));
+}
+
+void copy_BulletTargetingSystemOwner(BulletTargetingSystemOwner* to, BulletTargetingSystemOwner* from, bool to_game)
+{
+    memcpy(to->data_0, from->data_0, sizeof(to->data_0));
+}
+
+void copy_TargetingSystemBase(TargetingSystemBase* to, TargetingSystemBase* from, bool to_game)
+{
+    memcpy(to->data_0, from->data_0, sizeof(to->data_0));
+    to->data_1 = from->data_1;
+}
+
+void copy_BulletState(BulletState* to, BulletState* from, bool to_game)
+{
+    copy_BulletParamInfo(&to->paramInfo, &from->paramInfo, to_game);
+    to->data_0 = from->data_0;
+}
+
+void copy_BulletFlyState(BulletFlyState* to, BulletFlyState* from, bool to_game)
+{
+    copy_BulletState(&to->state, &from->state, to_game);
+    to->data_0 = from->data_0;
+}
+
+void copy_BulletParamInfo(BulletParamInfo* to, BulletParamInfo* from, bool to_game)
+{
+    memcpy(to->data_0, from->data_0, sizeof(to->data_0));
+}
+
+void copy_BulletMan_Field0x20(BulletMan_Field0x20* to, BulletMan_Field0x20* from, bool to_game)
+{
+    memcpy(to->data_0, from->data_0, sizeof(to->data_0));
+    copy_BulletParamInfo(to->bulletParamInfo, from->bulletParamInfo, to_game);
+    copy_BulletIns_Field0x90_Field0x1a0(&to->field0x1a0, &from->field0x1a0, to_game);
+    memcpy(to->data_1, from->data_1, sizeof(to->data_1));
+}
+
+BulletMan_Field0x20* init_BulletMan_Field0x20()
+{
+    BulletMan_Field0x20* local_BulletMan_Field0x20 = (BulletMan_Field0x20*)malloc_(sizeof(BulletMan_Field0x20));
+
+    local_BulletMan_Field0x20->bulletParamInfo = (BulletParamInfo*)malloc_(sizeof(BulletParamInfo));
+
+    return local_BulletMan_Field0x20;
+
+}
+
+void free_BulletMan_Field0x20(BulletMan_Field0x20* to, bool freeself)
+{
+    free(to->bulletParamInfo);
+
+    if (freeself)
+    {
+        free(to);
+    }
+}
+
+void copy_BulletMan_Field0x40(BulletMan_Field0x40* to, BulletMan_Field0x40* from, bool to_game)
+{
+    to->data_0 = from->data_0;
+    memcpy(to->data_1, from->data_1, sizeof(to->data_1));
+}
+
+BulletMan_Field0x40* init_BulletMan_Field0x40()
+{
+    BulletMan_Field0x40* local_BulletMan_Field0x40 = (BulletMan_Field0x40*)malloc_(sizeof(BulletMan_Field0x40));
+
+    return local_BulletMan_Field0x40;
+}
+
+void free_BulletMan_Field0x40(BulletMan_Field0x40* to, bool freeself)
+{
+    if (freeself)
+    {
+        free(to);
+    }
+}
+
+void copy_BulletMan_field0x78Elem(BulletMan_field0x78Elem* to, BulletMan_field0x78Elem* from, bool to_game)
+{
+    memcpy(to->data_0, from->data_0, sizeof(to->data_0));
+}
+
+BulletMan_field0x78Elem* init_BulletMan_field0x78Elem()
+{
+    BulletMan_field0x78Elem* local_BulletMan_field0x78Elem = (BulletMan_field0x78Elem*)malloc_(sizeof(BulletMan_field0x78Elem));
+
+    return local_BulletMan_field0x78Elem;
+}
+
+void free_BulletMan_field0x78Elem(BulletMan_field0x78Elem* to)
+{
+    free(to);
 }
