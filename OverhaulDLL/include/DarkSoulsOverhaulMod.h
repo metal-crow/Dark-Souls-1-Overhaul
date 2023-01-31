1b26782e064a928ede913330d4180ed9b5975c6e
metal-crow <frankcrisanti@aol.com>
2023-01-31 14:50:31 -0500

    version bump

--- OverhaulDLL/include/DarkSoulsOverhaulMod.h
+++ OverhaulDLL/include/DarkSoulsOverhaulMod.h
@@ -28,8 +28,8 @@
 // Preprocessor definition utilized by the Dark Souls Game Files C++ library (DsGameFiles.lib)
 #define __DS1_OVERHAUL_MOD
 
-static const uint64_t VERSION_RAW = 2023013100;
-#define VERSION_STRING  "Beta14"
+static const uint64_t VERSION_RAW = 2023013101;
+#define VERSION_STRING  "Beta15"
 #define DS1_OVERHAUL_TXT_INTRO "\n+------------------------------------------------------+\n|      Dark Souls Overhaul   ["VERSION_STRING"]                 |\n+------------------------------------------------------+"
 
 extern HMODULE d3d11_module;
